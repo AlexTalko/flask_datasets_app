963dc815608503cd9e7e39125f637ea2bd63a879
air <talko.alexander@gmail.com>
2025-04-16 23:45:36 +0400

    upd run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import os
+
 from app import create_app
 
 app = create_app()
 
 # Точка входа для запуска приложения
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=os.getenv('DEBUG'))
